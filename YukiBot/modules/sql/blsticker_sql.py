efd291b9408f79fd3385f5e9734b8377c0f8eb10
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-05-13 23:41:10 +0530

    Update blsticker_sql.py

--- YukiBot/modules/sql/blsticker_sql.py
+++ YukiBot/modules/sql/blsticker_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import BigInteger, Column, String, UnicodeText, distinct, func
 
-from MukeshRobot.modules.sql import BASE, SESSION
+from YukiBot.modules.sql import BASE, SESSION
 
 
 class StickersFilters(BASE):
