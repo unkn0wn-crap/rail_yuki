493cbc96cd386c5880ec158344e7097e61e4e451
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-05-20 00:05:58 +0530

    Update logo.py

--- YukiBot/modules/logo.py
+++ YukiBot/modules/logo.py
@@ -474,7 +474,7 @@ async def lego(event):
         draw.text(
             (x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black"
         )
-        fname = "mukesh.png"
+        fname = "yukibot.png"
         img.save(fname, "png")
         await telethn.send_file(
             event.chat_id,
