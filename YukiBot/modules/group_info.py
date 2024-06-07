509de1757ca2a5138eef68d9015384dbc0a7b98e
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-06-07 23:40:37 +0530

    Add files via upload

---
+++ YukiBot/modules/group_info.py
@@ -0,0 +1,68 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message 
+from YukiBot import pbot as app
+
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/Yukii_Onna_Bot?startgroup=true"),
+    ],
+]
+
+@app.on_message(filters.command("groupinfo", prefixes="/"))
+async def get_group_status(_, message: Message):
+    if len(message.command) != 2:
+        await message.reply("༗ Please provide a group username.⌥ Example ➾ `/groupinfo YourGroupUsername`")
+        return
+    
+    group_username = message.command[1]
+    
+    try:
+        group = await app.get_chat(group_username)
+    except Exception as e:
+        await message.reply(f"Error: {e}")
+        return
+    
+    total_members = await app.get_chat_members_count(group.id)
+    group_description = group.description
+    premium_acc = banned = deleted_acc = bot = 0  # You should replace these variables with actual counts.
+
+    response_text = (
+        f"༗ ɢʀᴏᴜᴘ ɴᴀᴍᴇ ➾ {group.title}\n\n"
+        f"⌥ ɢʀᴏᴜᴘ ɪᴅ ➾ `{group.id}`\n"
+        f"⌥ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs ➾ {total_members}\n"
+        f"⌥ ᴜsᴇʀɴᴀᴍᴇ ➾ @{group_username}\n"
+        f"⌥ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ➾ \n{group_description or 'N/A'}\n\n"
+        f"༗ 🄿🄰🅁🄰🄳🄾🅇"
+    )
+    
+    await message.reply((response_text),reply_markup=InlineKeyboardMarkup(EVAA),)
+
+
+
+
+
+
+# Command handler to get group status
+@app.on_message(filters.command("status") & filters.group)
+def group_status(client, message):
+    chat = message.chat  # Chat where the command was sent
+    status_text = f"⌥ ɢʀᴏᴜᴘ ɪᴅ ➾ `{chat.id}`\n" \
+                  f"⌥ ᴛɪᴛʟᴇ ➾ {chat.title}\n" \
+                  f"⌥ ᴛʏᴘᴇ ➾ {chat.type}\n"
+                  
+    if chat.username:  # Not all groups have a username
+        status_text += f"⌥ ᴜsᴇʀɴᴀᴍᴇ ➾ @{chat.username}\n\n༗ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➾ ๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐"
+    else:
+        status_text += "Username: None"
+
+    message.reply_text((status_text),reply_markup=InlineKeyboardMarkup(EVAA),)
+    
+
+
+__help__ = """
+
+ ⬤ /groupinfo ➾ ɢᴇᴛ ɢʀᴏᴜᴘ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
+ ⬤ /groupdata ➾ ɢᴇᴛ ɢʀᴏᴜᴘ ᴅᴀᴛᴀ.
+"""
+
+__mod_name__ = "ɢʀᴏᴜᴘ"
---
+++ YukiBot/modules/v_song.py
@@ -0,0 +1,117 @@
+from __future__ import unicode_literals
+
+import asyncio
+import os
+import time
+from urllib.parse import urlparse
+
+import wget
+from pyrogram import filters
+from pyrogram.types import Message
+from youtubesearchpython import SearchVideos
+from yt_dlp import YoutubeDL
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from YukiBot import pbot
+
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ..", url=f"https://t.me/Yukii_Onna_Bot?startgroup=true"),
+    ],
+]
+
+
+def get_file_extension_from_url(url):
+    url_path = urlparse(url).path
+    basename = os.path.basename(url_path)
+    return basename.split(".")[-1]
+
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+
+@pbot.on_message(filters.command(["vsong", "video"]))
+async def ytmusic(client, message: Message):
+    urlissed = get_text(message)
+    await message.delete()
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    chutiya = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+
+    pablo = await client.send_message(message.chat.id, f"📺")
+    if not urlissed:
+        await pablo.edit(
+            "❍ sᴏɴɢ ɴᴏᴛ ғᴏᴜɴᴅ ᴏɴ ʏᴏᴜᴛᴜʙᴇ, ᴄʜᴋ ᴀɢᴀɪɴ."
+        )
+        return
+
+    search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    mo = mio[0]["link"]
+    thum = mio[0]["title"]
+    fridayz = mio[0]["id"]
+    thums = mio[0]["channel"]
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    url = mo
+    sedlyf = wget.download(kekme)
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+        "outtmpl": "%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            infoo = ytdl.extract_info(url, False)
+            round(infoo["duration"] / 60)
+            ytdl_data = ytdl.extract_info(url, download=True)
+
+    except Exception as e:
+        await pablo.edit(f"**❍ ғᴀɪʟᴇᴅ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ.** \n**❅ ᴇʀʀᴏʀ ➾** `{str(e)}`")
+        return
+    c_time = time.time()
+    file_stark = f"{ytdl_data['id']}.mp4"
+    capy = f"༗ **sᴏɴɢ ᴛɪᴛʟᴇ ➾** [{thum}]({mo})\n\n⌥ **ᴄʜᴀɴɴᴇʟ ➾** {thums}\n⌥ **sᴇᴀʀᴄʜᴇᴅ ➾** {urlissed}\n\n 🄿🄰🅁🄰🄳🄾🅇"
+    await client.send_video(
+        message.chat.id,
+        video=open(file_stark, "rb"),
+        duration=int(ytdl_data["duration"]),
+        file_name=str(ytdl_data["title"]),
+        thumb=sedlyf,
+        caption=capy,
+        supports_streaming=True,
+        progress_args=(
+            pablo,
+            c_time,
+            f"❍ ᴩʟᴇᴀsᴇ ᴡᴀɪᴛ...\n\n❅ ᴜᴩʟᴏᴀᴅɪɴɢ `{urlissed}` ғʀᴏᴍ ʏᴏᴜᴛᴜʙᴇ sᴇʀᴠᴇʀs...💫",
+            file_stark,
+        ), reply_markup=InlineKeyboardMarkup(EVAA),
+    )
+    await pablo.delete()
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+
+__mod_name__ = "ᴠɪᴅᴇᴏ"
+
+
+  
