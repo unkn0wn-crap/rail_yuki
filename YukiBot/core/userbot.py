41ae2fff464d1709009afe1d0cbf36077775d9f6
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-06-08 11:08:06 +0530

    Update userbot.py

--- YukiBot/core/userbot.py
+++ YukiBot/core/userbot.py
@@ -1,54 +1,177 @@
-from pyrogram import Client, errors
-from pyrogram.enums import ChatMemberStatus, ParseMode
+from pyrogram import Client
 
 import config
 
 from ..logging import LOGGER
 
+assistants = []
+assistantids = []
 
-class DAXX(Client):
+
+class Userbot(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"❖ Starting Bot...💞")
-        super().__init__(
-            name="DAXXMUSIC",
+        self.one = Client(
+            name="YUKIss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            max_concurrent_transmissions=7,
+            session_string=str(config.STRING1),
+            no_updates=True,
+        )
+        self.two = Client(
+            name="YUKIss2",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING2),
+            no_updates=True,
+        )
+        self.three = Client(
+            name="YUKIss3",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING3),
+            no_updates=True,
+        )
+        self.four = Client(
+            name="YUKIss4",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING4),
+            no_updates=True,
+        )
+        self.five = Client(
+            name="YUKIss5",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            session_string=str(config.STRING5),
+            no_updates=True,
         )
 
     async def start(self):
-        await super().start()
-        self.id = self.me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.username = self.me.username
-        self.mention = self.me.mention
+        LOGGER(__name__).info(f" sᴛᴀʀᴛɪɴɢ ᴀssɪsᴛᴀɴᴛs...")
+        if config.STRING1:
+            await self.one.start()
+            try:
+                await self.one.join_chat("Blyat_u")
 
-        try:
-            await self.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"<b>❖ {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ ⏤͟͟͞͞★</b>\n\n● ɪᴅ ➥ <code>{self.id}</code>\n● ɴᴀᴍᴇ ➥ {self.name}\n● ᴜsᴇʀɴᴀᴍᴇ ➥ @{self.username}",
-            )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(__name__).error(
-                "❖ Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel...💙"
-            )
-            exit()
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"❖ Bot has failed to access the log group/channel.\n● Reason ➥ {type(ex).__name__}...💛"
-            )
-            exit()
+                await self.one.join_chat("paradoxdump")
+            except:
+                pass
+            assistants.append(1)
+            try:
+                await self.one.send_message(config.LOGGER_ID, " ᴀssɪsᴛᴀɴᴛ sᴛᴀʀᴛ...")
 
-        a = await self.get_chat_member(config.LOGGER_ID, self.id)
-        if a.status != ChatMemberStatus.ADMINISTRATOR:
-            LOGGER(__name__).error(
-                "❖ Please promote your bot as an admin in your log group/channel...💚"
-            )
-            exit()
-        LOGGER(__name__).info(f"❖ Music Bot Started as {self.name} ...🧡")
+            except:
+                LOGGER(__name__).error(
+                    "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                )
+                exit()
+            self.one.id = self.one.me.id
+            self.one.name = self.one.me.mention
+            self.one.username = self.one.me.username
+            assistantids.append(self.one.id)
+            LOGGER(__name__).info(f" Assistant Started as {self.one.name}")
+
+        if config.STRING2:
+            await self.two.start()
+            try:
+                await self.two.join_chat("Blyat_u")
+
+                await self.one.join_chat("paradoxdump")
+            except:
+                pass
+            assistants.append(2)
+            try:
+                await self.two.send_message(config.LOGGER_ID, " ᴀssɪsᴛᴀɴᴛ sᴛᴀʀᴛᴇᴅ...")
+            except:
+                LOGGER(__name__).error(
+                    "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                )
+                exit()
+            self.two.id = self.two.me.id
+            self.two.name = self.two.me.mention
+            self.two.username = self.two.me.username
+            assistantids.append(self.two.id)
+            LOGGER(__name__).info(f" Assistant Two Started as {self.two.name}")
+
+        if config.STRING3:
+            await self.three.start()
+            try:
+                await self.three.join_chat("Blyat_u")
+
+                await self.one.join_chat("paradoxdump")
+            except:
+                pass
+            assistants.append(3)
+            try:
+                await self.three.send_message(config.LOGGER_ID, " ᴀssɪsᴛᴀɴᴛ sᴛᴀʀᴛᴇᴅ...")
+            except:
+                LOGGER(__name__).error(
+                    "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+                exit()
+            self.three.id = self.three.me.id
+            self.three.name = self.three.me.mention
+            self.three.username = self.three.me.username
+            assistantids.append(self.three.id)
+            LOGGER(__name__).info(f" Assistant Three Started as {self.three.name}")
+
+        if config.STRING4:
+            await self.four.start()
+            try:
+                await self.four.join_chat("Blyat_u")
+
+                await self.one.join_chat("paradoxdump")
+            except:
+                pass
+            assistants.append(4)
+            try:
+                await self.four.send_message(config.LOGGER_ID, " ᴀssɪsᴛᴀɴᴛ sᴛᴀʀᴛᴇᴅ...")
+            except:
+                LOGGER(__name__).error(
+                    "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                )
+                exit()
+            self.four.id = self.four.me.id
+            self.four.name = self.four.me.mention
+            self.four.username = self.four.me.username
+            assistantids.append(self.four.id)
+            LOGGER(__name__).info(f" Assistant Four Started as {self.four.name}")
+
+        if config.STRING5:
+            await self.five.start()
+            try:
+                await self.five.join_chat("Blyat_u")
+
+                await self.one.join_chat("paradoxdump")
+            except:
+                pass
+            assistants.append(5)
+            try:
+                await self.five.send_message(config.LOGGER_ID, " ᴀssɪsᴛᴀɴᴛ sᴛᴀʀᴛᴇᴅ...")
+            except:
+                LOGGER(__name__).error(
+                    "๏ ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ 5 ʜᴀs ғᴀɪʟᴇᴅ ᴛᴏ ᴀᴄᴄᴇss ᴛʜᴇ ʟᴏɢ ɢʀᴏᴜᴘ. ᴍᴀᴋᴇ sᴜʀᴇ ᴛʜᴀᴛ ʏᴏᴜ ʜᴀᴠᴇ ᴀᴅᴅᴇᴅ ʏᴏᴜʀ ᴀssɪsᴛᴀɴᴛ ᴛᴏ ʏᴏᴜʀ ʟᴏɢ ɢʀᴏᴜᴘ ᴀɴᴅ ᴘʀᴏᴍᴏᴛᴇᴅ ᴀs ᴀᴅᴍɪɴ "
+                )
+                exit()
+            self.five.id = self.five.me.id
+            self.five.name = self.five.me.mention
+            self.five.username = self.five.me.username
+            assistantids.append(self.five.id)
+            LOGGER(__name__).info(f" Assistant Five Started as {self.five.name}")
 
     async def stop(self):
-        await super().stop()
-      
+        LOGGER(__name__).info(f" sᴛᴏᴘᴘɪɴɢ ᴀssɪsᴛᴀɴᴛs...")
+        try:
+            if config.STRING1:
+                await self.one.stop()
+            if config.STRING2:
+                await self.two.stop()
+            if config.STRING3:
+                await self.three.stop()
+            if config.STRING4:
+                await self.four.stop()
+            if config.STRING5:
+                await self.five.stop()
+        except:
+            pass
+
