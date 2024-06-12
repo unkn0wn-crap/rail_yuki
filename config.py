382671028e4e23964a4a4c985f1615d9703fa07b
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-06-12 09:44:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+-1002092954715
+import re
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+# Chat id of a group for logging bot's activities
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002092954715"))
