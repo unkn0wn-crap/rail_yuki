0d196fc8ba7dd45e047d7ad57d3810b2d6c07e2d
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-06-13 15:25:39 +0530

    Update __init__.py

--- YukiBot/modules/sql/__init__.py
+++ YukiBot/modules/sql/__init__.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from YukiBot import DB_URI
 from YukiBot import LOGGER as log
+
+# Update your DB_URI here
+DB_URI = "postgres://koyeb-adm:gRf7YuhG8ZIm@ep-shy-hill-a42qjv7m.us-east-1.pg.koyeb.app/koyebdb"
 
 if DB_URI and DB_URI.startswith("postgres://"):
     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
