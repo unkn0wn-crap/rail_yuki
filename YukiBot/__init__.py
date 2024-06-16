c13b8da53d414263f53108a3e777bde6591e1fba
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-06-16 16:45:54 +0530

    Update __init__.py

--- YukiBot/__init__.py
+++ YukiBot/__init__.py
@@ -72,23 +72,23 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
     try:
-        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "6195725562").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "6195725562").split())
+        DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "6144093089").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
     try:
-        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "6195725562").split())
+        DEMONS = set(int(x) for x in os.environ.get("DEMONS", "").split())
     except ValueError:
         raise Exception("Your support users list does not contain valid integers.")
 
     try:
-        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "6195725562").split())
+        TIGERS = set(int(x) for x in os.environ.get("TIGERS", "").split())
     except ValueError:
         raise Exception("Your tiger users list does not contain valid integers.")
 
     try:
-        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "6195725562").split())
+        WOLVES = set(int(x) for x in os.environ.get("WOLVES", "").split())
     except ValueError:
         raise Exception("Your whitelisted users list does not contain valid integers.")
 
