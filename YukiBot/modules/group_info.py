65e34e7ffe72f93081f171ec9641f06e9d0c87be
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-06-16 17:10:00 +0530

    Update group_info.py

--- YukiBot/modules/group_info.py
+++ YukiBot/modules/group_info.py
@@ -11,17 +11,21 @@ EVAA = [
 @app.on_message(filters.command("groupinfo", prefixes="/"))
 async def get_group_status(_, message: Message):
     if len(message.command) != 2:
-        await message.reply("༗ Please provide a group username.⌥ Example ➾ `/groupinfo YourGroupUsername`")
+        await message.reply("༗ Please provide a group username or chat ID.⌥ Example ➾ `/groupinfo YourGroupUsername` or `/groupinfo -1001234567890`")
         return
-    
-    group_username = message.command[1]
-    
+
+    group_identifier = message.command[1]
+
     try:
-        group = await app.get_chat(group_username)
+        if group_identifier.startswith("-100") and group_identifier[1:].isdigit():
+            group_id = int(group_identifier)
+            group = await app.get_chat(group_id)
+        else:
+            group = await app.get_chat(group_identifier)
     except Exception as e:
         await message.reply(f"Error: {e}")
         return
-    
+
     total_members = await app.get_chat_members_count(group.id)
     group_description = group.description
     premium_acc = banned = deleted_acc = bot = 0  # You should replace these variables with actual counts.
@@ -30,17 +34,12 @@ async def get_group_status(_, message: Message):
         f"༗ ɢʀᴏᴜᴘ ɴᴀᴍᴇ ➾ {group.title}\n\n"
         f"⌥ ɢʀᴏᴜᴘ ɪᴅ ➾ `{group.id}`\n"
         f"⌥ ᴛᴏᴛᴀʟ ᴍᴇᴍʙᴇʀs ➾ {total_members}\n"
-        f"⌥ ᴜsᴇʀɴᴀᴍᴇ ➾ @{group_username}\n"
+        f"⌥ ᴜsᴇʀɴᴀᴍᴇ ➾ @{group.username if group.username else 'N/A'}\n"
         f"⌥ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ➾ \n{group_description or 'N/A'}\n\n"
         f"༗ 🄿🄰🅁🄰🄳🄾🅇"
     )
-    
-    await message.reply((response_text),reply_markup=InlineKeyboardMarkup(EVAA),)
 
-
-
-
-
+    await message.reply((response_text), reply_markup=InlineKeyboardMarkup(EVAA))
 
 # Command handler to get group status
 @app.on_message(filters.command("status") & filters.group)
@@ -49,18 +48,15 @@ def group_status(client, message):
     status_text = f"⌥ ɢʀᴏᴜᴘ ɪᴅ ➾ `{chat.id}`\n" \
                   f"⌥ ᴛɪᴛʟᴇ ➾ {chat.title}\n" \
                   f"⌥ ᴛʏᴘᴇ ➾ {chat.type}\n"
-                  
+
     if chat.username:  # Not all groups have a username
         status_text += f"⌥ ᴜsᴇʀɴᴀᴍᴇ ➾ @{chat.username}\n\n"
     else:
         status_text += "Username: None"
 
-    message.reply_text((status_text),reply_markup=InlineKeyboardMarkup(EVAA),)
-    
-
+    message.reply_text((status_text), reply_markup=InlineKeyboardMarkup(EVAA))
 
 __help__ = """
-
  ⬤ /groupinfo ➾ ɢᴇᴛ ɢʀᴏᴜᴘ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
  ⬤ /groupdata ➾ ɢᴇᴛ ɢʀᴏᴜᴘ ᴅᴀᴛᴀ.
 """
