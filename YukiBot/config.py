b9a6191c99243efb9c6538cebad352d965092035
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-06-30 15:35:56 +0530

    Update config.py

--- YukiBot/config.py
+++ YukiBot/config.py
@@ -1,61 +1,62 @@
+import os
 
 class Config(object):
     LOGGER = True
 
     #####
 
-    ANILIST_CLIENT = getenv("ANILIST_CLIENT", "8679")
+    ANILIST_CLIENT = os.getenv("ANILIST_CLIENT", "8679")
   
-    ANILIST_SECRET = getenv("ANILIST_SECRET", "NeCEq9A1hVnjsjZlTZyNvqK11krQ4HtSliaM7rTN")
+    ANILIST_SECRET = os.getenv("ANILIST_SECRET", "NeCEq9A1hVnjsjZlTZyNvqK11krQ4HtSliaM7rTN")
   
-    API_ID = getenv("API_ID", None)
+    API_ID = os.getenv("API_ID", "6435225")
    
-    API_HASH = getenv("API_HASH",None)
+    API_HASH = os.getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
    
-    TOKEN = getenv("TOKEN", None)
+    TOKEN = os.getenv("TOKEN", "7134066784:AAGVKa8zcfdteUF5jCyKRlSMG7mhlvEecqY")
   
-    OWNER_ID = getenv("OWNER_ID", "6259443940") 
+    OWNER_ID = os.getenv("OWNER_ID", "6259443940") 
 
 
     OWNER_USERNAME = ("OWNER_USERNAME", "corpsealone")
     
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "paradoxdump")
+    SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "paradoxdump")
    
-    START_IMG = getenv("START_IMG", "https://telegra.ph/file/56c9da084a528eac54142.jpg")
+    START_IMG = os.getenv("START_IMG", "https://telegra.ph/file/56c9da084a528eac54142.jpg")
 
-    JOIN_LOGGER = getenv("JOIN_LOGGER", "-1002092954715")
+    JOIN_LOGGER = os.getenv("JOIN_LOGGER", "-1002092954715")
    
-    EVENT_LOGS = getenv("EVENT_LOGS",  "-1002092954715")
+    EVENT_LOGS = os.getenv("EVENT_LOGS",  "-1002092954715")
   
-    ERROR_LOGS = getenv("ERROR_LOGS", "-1002092954715")
+    ERROR_LOGS = os.getenv("ERROR_LOGS", "-1002092954715")
 
-    MONGO_DB_URI= getenv("MONGO_DB_URI", None)
+    MONGO_DB_URI= os.getenv("MONGO_DB_URI", "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority")
    
-    LOG_CHANNEL = getenv("LOG_CHANNEL", "-1002092954715")
+    LOG_CHANNEL = os.getenv("LOG_CHANNEL", "-1002092954715")
    
-    BOT_USERNAME = getenv("BOT_USERNAME" , "Yuki_Obot")
+    BOT_USERNAME = os.getenv("BOT_USERNAME" , "Yukii_Onna_bot")
    
-    DATABASE_URL = getenv("DATABASE_URL", "postgres://iarfggbc:Vxzh_kG7cxa1kHR5faxcd1kuA4R-UT9E@rosie.db.elephantsql.com/iarfggbc")
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgres://pgcyxnql:Lc5Ix0fJpHJ3hIpkjLzUK8cy2KPEw564@hansken.db.elephantsql.com/pgcyxnql")
 
-    CASH_API_KEY = getenv("CASH_API_KEY", "V48U2FLLKRHSVD4X")
+    CASH_API_KEY = os.getenv("CASH_API_KEY", "V48U2FLLKRHSVD4X")
     
-    TIME_API_KEY = getenv("TIME_API_KEY", "1CUBX1HXGNHW")
+    TIME_API_KEY = os.getenv("TIME_API_KEY", "1CUBX1HXGNHW")
 
-    SPAMWATCH_API = getenv("SPAMWATCH_API", "3624487efd8e4ca9c949f1ab99654ad1e4de854f41a14afd00f3ca82d808dc8c")
+    SPAMWATCH_API = os.getenv("SPAMWATCH_API", "3624487efd8e4ca9c949f1ab99654ad1e4de854f41a14afd00f3ca82d808dc8c")
     
-    SPAMWATCH_SUPPORT_CHAT = getenv("SPAMWATCH_SUPPORT_CHAT", "paradoxdump")
+    SPAMWATCH_SUPPORT_CHAT = os.getenv("SPAMWATCH_SUPPORT_CHAT", "paradoxdump")
     
-    WALL_API = getenv("WALL_API", "2455acab48f3a935a8e703e54e26d121")
+    WALL_API = os.getenv("WALL_API", "2455acab48f3a935a8e703e54e26d121")
     
-    REM_BG_API_KEY = getenv("REM_BG_API_KEY", "xYCR1ZyK3ZsofjH7Y6hPcyzC")
+    REM_BG_API_KEY = os.getenv("REM_BG_API_KEY", "xYCR1ZyK3ZsofjH7Y6hPcyzC")
     
-    OPENWEATHERMAP_ID = getenv("OPENWEATHERMAP_ID", "887da2c60d9f13fe78b0f9d0c5cbaade")
+    OPENWEATHERMAP_ID = os.getenv("OPENWEATHERMAP_ID", "887da2c60d9f13fe78b0f9d0c5cbaade")
 
-    BAN_STICKER = getenv("BAN_STICKER", "CAACAgUAAxkBAAJW8WZDBQe8YG5vZWXx9E2ILluzxMKsAAJZDQACluSpVdFgrIHsInutNQQ")
+    BAN_STICKER = os.getenv("BAN_STICKER", "CAACAgUAAxkBAAJW8WZDBQe8YG5vZWXx9E2ILluzxMKsAAJZDQACluSpVdFgrIHsInutNQQ")
 
-    HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+    HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME", None)
 
-    HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+    HEROKU_API_KEY = os.getenv("HEROKU_API_KEY", None)
     
     # Optional fields
     
