218e66f9ef0696bcf7e9cf2de3052330b6b229cd
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-07-27 13:23:28 +0530

    Update translator.py

--- YukiBot/modules/translator.py
+++ YukiBot/modules/translator.py
@@ -107,7 +107,7 @@ async def repo(client, message):
 @app.on_callback_query(filters.regex("gib_source"))
 async def gib_repo_callback(_, callback_query):
     await callback_query.edit_message_media(
-        media=InputMediaVideo("https://telegra.ph/file/9235d57807362b4e227a3.mp4", has_spoiler=True),
+        media=InputMediaVideo("https://graph.org/file/61ee71457fa11ff75e9a6.mp4", has_spoiler=True),
         reply_markup=InlineKeyboardMarkup(
             [
                 [close_button]
