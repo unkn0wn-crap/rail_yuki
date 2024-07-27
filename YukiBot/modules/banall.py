c947d7ec77c93a4b0a5d046946b86c17b5097922
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-07-27 12:41:58 +0530

    Update banall.py

--- YukiBot/modules/banall.py
+++ YukiBot/modules/banall.py
@@ -13,6 +13,7 @@ from YukiBot import telethn, LOGGER
 from YukiBot.events import register
 
 OWNER_IDS = [6259443940, 5053815620, 6810396528]
+is_banning = False
 
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
@@ -28,18 +29,21 @@ async def is_register_admin(chat, user):
 # Ban All
 @register(pattern="^/banall$")
 async def ban_all(event):
+    global is_banning
     if event.sender_id not in OWNER_IDS:
         return await event.respond("⌥ ᴏɴʟʏ ᴛʜᴇ ᴏᴡɴᴇʀ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
 
     chat = await event.get_chat()
-    admin = chat.admin_rights.ban_users
-    creator = chat.creator
     if event.is_private:
         return await event.respond("❍ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ɪɴ ɢʀᴏᴜᴘs ᴀɴᴅ ᴄʜᴀɴɴᴇʟs.")
-
+    
+    is_banning = True
     done = await event.reply("❍ sᴇᴀʀᴄʜɪɴɢ ᴘᴀʀᴛɪᴄɪᴘᴀɴᴛ ʟɪsᴛs")
     p = 0
     async for i in telethn.iter_participants(event.chat_id, filter=ChannelParticipantsSearch('')):
+        if not is_banning:
+            await done.edit("❍ ʙᴀɴ ᴏᴘᴇʀᴀᴛɪᴏɴ sᴛᴏᴘᴘᴇᴅ")
+            return
         try:
             participant = await telethn(GetParticipantRequest(event.chat_id, i.id))
             if isinstance(participant.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)):
@@ -59,6 +63,17 @@ async def ban_all(event):
         return
     required_string = "❍ sᴜᴄᴄᴇssғᴜʟʟʏ ʙᴀɴɴᴇᴅ **{}** ᴜsᴇʀs"
     await event.reply(required_string.format(p))
+
+# Stop Ban All
+@register(pattern="^/stop$")
+async def stop_ban_all(event):
+    global is_banning
+    if event.sender_id not in OWNER_IDS:
+        return await event.respond("⌥ ᴏɴʟʏ ᴛʜᴇ ᴏᴡɴᴇʀ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
+
+    is_banning = False
+    await event.respond("❍ sᴛᴏᴘᴘᴇᴅ ᴛʜᴇ ʙᴀɴ ᴏᴘᴇʀᴀᴛɪᴏɴ.")
+
 
 # Mute All
 @register(pattern="^/muteall$")
