da8b50a1fc7bf0d87b91ca9b9ad22fb50c451535
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-08-06 09:23:11 +0530

    Update config.py

--- YukiBot/config.py
+++ YukiBot/config.py
@@ -61,11 +61,11 @@ class Config(object):
     # Optional fields
     
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
+    DRAGONS = [6810396528]  # User id of sudo users
+    DEV_USERS = [6810396528]  # User id of dev users
     DEMONS = []  # User id of support users
     TIGERS = []  # User id of tiger users
-    WOLVES = []  # User id of whitelist users
+    WOLVES = [6810396528, 6259443940]  # User id of whitelist users
 
     ALLOW_CHATS = True
     ALLOW_EXCL = True
