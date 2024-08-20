57971efa49214644d1b791bdfab96ae8e06e6297
corpse-x <123547302+corpse-x@users.noreply.github.com>
2024-08-20 11:09:59 +0530

    Update chatbot_sql.py

--- YukiBot/modules/sql/chatbot_sql.py
+++ YukiBot/modules/sql/chatbot_sql.py
@@ -5,38 +5,38 @@ from sqlalchemy import Column, String
 from YukiBot.modules.sql import BASE, SESSION
 
 
-class MukeshChats(BASE):
-    __tablename__ = "mukesh_chats"
+class YukiChats(BASE):
+    __tablename__ = "yuki_chats"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
 
-MukeshChats.__table__.create(checkfirst=True)
+YukiChats.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def is_mukesh(chat_id):
+def is_yuki(chat_id):
     try:
-        chat = SESSION.query(MukeshChats).get(str(chat_id))
+        chat = SESSION.query(YukiChats).get(str(chat_id))
         return bool(chat)
     finally:
         SESSION.close()
 
 
-def set_mukesh(chat_id):
+def set_yuki(chat_id):
     with INSERTION_LOCK:
-        mukeshchat = SESSION.query(MukeshChats).get(str(chat_id))
-        if not mukeshchat:
-            mukeshchat = MukeshChats(str(chat_id))
-        SESSION.add(mukeshchat)
+        yukichat = SESSION.query(YukiChats).get(str(chat_id))
+        if not yukichat:
+            yukichat = YukiChats(str(chat_id))
+        SESSION.add(yukichat)
         SESSION.commit()
 
 
-def rem_mukesh(chat_id):
+def rem_yuki(chat_id):
     with INSERTION_LOCK:
-        mukeshchat = SESSION.query(MukeshChats).get(str(chat_id))
-        if mukeshchat:
-            SESSION.delete(mukeshchat)
+        yukichat = SESSION.query(YukiChats).get(str(chat_id))
+        if yukichat:
+            SESSION.delete(yukichat)
         SESSION.commit()
